c841337b9e84aed35583ebbc76eb0df17d9442db
Penguin <ishanjaiswal370@gmail.com>
2024-02-01 18:23:10 +0530

    First Commit

---
+++ src/routes/report.js
@@ -0,0 +1,11 @@
+const getReport = (req, res, next) => {
+	const { uid, title, desc, time, location } = req.body;
+};
+
+const getLink = (req, res, next) => {};
+
+const genDesc = (req, res, next) => {};
+
+const publishToDb = (req, res, next) => {};
+
+module.exports = { report, r1 };
--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const colors = require("@colors/colors");
 const dotenv = require("dotenv").config();
+const { report, r1 } = require("./routes/report");
+const { upload } = require("./middleware/imageUpload");
 
 const app = express();
 const port = process.env.PORT || 6969;
 
-app.route("/").put();
-
-app.get("/", (req, res) => {
-    res.send("Penguin Pro🐧")
-})
+app.route("/").get(upload.single("img"), [report, r1]);  
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`.cyan);
